d184e548411c17dc925510f484bcd05bf6f068b0
Fix1t <gbiel1117@gmail.com>
2023-02-17 18:24:39 +0100

    ADDED: header prepareLine parseBody-base

--- parser.php
+++ parser.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+//error codes
+define("MISSING_HEADER",21);
+define("PARAMETER_ERROR",10);
+
 class ArgumentParser {
     private $numberOfArguments;
     private $arguments;
@@ -37,6 +41,7 @@ class ArgumentParser {
             $curArgument = $this->getNextArgument();
             if ($curArgument == null) {
                 $moreArguments = false;
+                //skips parsing when there is no more arguments
                 continue;
             }
             //Cuts to position of equals in the string, last character if equals is not in the string
@@ -48,13 +53,13 @@ class ArgumentParser {
                     //there should not be any onther argument besides --help
                     $ALLOWED_ARGS_WITH_HELP = 2;
                     if ($ALLOWED_ARGS_WITH_HELP != $this->numberOfArguments)
-                        exit(10);
+                        exit(PARAMETER_ERROR);
                     else
                         exit(0);
 
                 default: 
                     //other parameters are not allowed
-                    exit(10);
+                    exit(PARAMETER_ERROR);
             }
             
         } while ($moreArguments == true);
@@ -63,25 +68,63 @@ class ArgumentParser {
 
 
 class mainParser{
-    private $input;
 
-    public function __construct($stdin){
-        $input = $stdin;
-    }
+    private $stdin;
+    private $curLine;
 
     private function header(){
-
+        $this->prepareLine();
+        $prolog = ".IPPcode23";
+        if ($this->curLine != $prolog) {
+            echo "Missling prolog.";
+            exit(MISSING_HEADER);
+        }
+        /*  */
+        echo "DEVEL: prolog found";
     }
 
     private function move(){
 
     }
+
+    private function defvar(){
+        
+    }
     // ...
 
-    public function parse(){
-        $line = fgets($this->input);
+    private function prepareLine(){
+        $line = fgets($this->stdin);
+        $commentPos = strpos($line,'#');
+        if ($commentPos) {
+            //remove any comment
+            $line = substr($line,0,$commentPos);
+        }
+        $line = trim($line);
+        $this->curLine = $line;
+    }
 
+    private function parseBody(){
+        $this->prepareLine();
+        $this->curLine = explode(" ",$this->curLine);
+        switch ($this->curLine[0]) {
+            case 'DEFVAR':
+                $this->defvar();
+                break;
+            
+            default:
+                
+                break;
+        }
 
+    }
+
+    public function parse($stdin){
+        $this->stdin = $stdin;
+
+        $this->header();
+
+        $this->parseBody();
+        
     }
 
 }
@@ -91,5 +134,6 @@ $argumentParser = new ArgumentParser($argc,$argv);
 
 $argumentParser->parseArguments();
 
-$parser = new mainParser(fopen("php://stdin","r"));
+$parser = new mainParser();
 
+$parser->parse(fopen("php://stdin","r"));
