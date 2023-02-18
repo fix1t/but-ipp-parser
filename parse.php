96b41d2d7183718e6b87f87ecdadc947ae0c1a49
Fix1t <gbiel1117@gmail.com>
2023-02-18 12:32:54 +0100

    DONE: Basic

--- parse.php
+++ parse.php
@@ -1,49 +1,56 @@
 <?php
+
 declare(strict_types=1);
 
 //error codes
 // define("DEVEL","\t[DEVEL]: ");
-define("SUCCESS",0);
-define("PARAMETER_ERROR",10);
-define("HEADER_ERROR",21);
-define("UNEXPECTED_COMMAND",22);
-define("LEXICAL_ERROR",23);
-define("SYNTAX_ERROR",23);
+define("SUCCESS", 0);
+define("PARAMETER_ERROR", 10);
+define("HEADER_ERROR", 21);
+define("UNEXPECTED_COMMAND", 22);
+define("LEXICAL_ERROR", 23);
+define("SYNTAX_ERROR", 23);
 
 
-class ArgumentParser {
+class ArgumentParser
+{
     private $numberOfArguments;
     private $arguments;
     private $argumentsDone = 1; //skip the firs one.
 
-    public function __construct($argc, $argv) {
-        $this->numberOfArguments= $argc;
+    public function __construct($argc, $argv)
+    {
+        $this->numberOfArguments = $argc;
         $this->arguments = $argv;
     }
-    public function getNumberOfArgumets(){
+    public function getNumberOfArgumets()
+    {
         return $this->numberOfArguments;
     }
-    public function getArguments(){
+    public function getArguments()
+    {
         return $this->arguments;
     }
 
-    public function getNextArgument(){
+    public function getNextArgument()
+    {
         if ($this->argumentsDone != $this->numberOfArguments) {
             return $this->arguments[$this->argumentsDone++];
-        }
-        else {
+        } else {
             return null;
         }
     }
 
     /* TODO */
-    public function argHelp(){
-        echo("Usage: [...] ...\n");
+    public function argHelp()
+    {
+        echo ("Usage: [...] ...\n");
     }
 
-    public function parseArguments(){
+    public function parseArguments()
+    {
         $moreArguments = true;
-        do{
+        do {
             $curArgument = $this->getNextArgument();
             if ($curArgument == null) {
                 $moreArguments = false;
@@ -51,8 +58,8 @@ class ArgumentParser {
                 continue;
             }
             //Cuts to position of equals in the string, last character if equals is not in the string
-            $toEquals = strpos($curArgument, '=')?strpos($curArgument, '='):strlen($curArgument);
-            
+            $toEquals = strpos($curArgument, '=') ? strpos($curArgument, '=') : strlen($curArgument);
+
             switch (substr($curArgument, 0, $toEquals)) {
                 case '--help':
                     $this->argHelp();
@@ -63,30 +70,31 @@ class ArgumentParser {
                     else
                         exit(SUCCESS);
 
-                default: 
+                default:
                     //other parameters are not allowed
                     exit(PARAMETER_ERROR);
             }
-            
         } while ($moreArguments == true);
     }
 }
 
 
-class mainParser{
+class mainParser
+{
 
     private $stdin;
     private $curLine;
     private $instructionOrder = 1;
 
-    private function header(){
+    private function header()
+    {
         //check if there is a prolog on the input
         $this->prepareLine();
-        
+
         //empty input
         if ($this->curLine === false)
             exit(HEADER_ERROR);
-        
+
         $prolog = ".IPPcode23";
         if ($this->curLine != $prolog) {
             // echo "[DEVEL]: Missling prolog.\n";
@@ -99,47 +107,48 @@ class mainParser{
         echo "<program language=\"IPPcode23\">\n";
     }
 
-    private function parseString($string){
-        while (($position = strpos($string,'&')) !== false) {
-            $string = substr($string, 0, $position) . '&amp;' . substr($string, $position+1);
+    private function parseString($string)
+    {
+        while (($position = strpos($string, '&')) !== false) {
+            $string = substr($string, 0, $position) . '&amp;' . substr($string, $position + 1);
         }
-        while (($position = strpos($string,'<')) !== false) {
-            $string = substr($string, 0, $position) . '&lt;' . substr($string, $position+1);
+        while (($position = strpos($string, '<')) !== false) {
+            $string = substr($string, 0, $position) . '&lt;' . substr($string, $position + 1);
         }
-        while (($position = strpos($string,'>')) !== false) {
-            $string = substr($string, 0, $position) . '&gt;' . substr($string, $position+1);
+        while (($position = strpos($string, '>')) !== false) {
+            $string = substr($string, 0, $position) . '&gt;' . substr($string, $position + 1);
         }
         return $string;
     }
 
-    private function parseInstructionArgument($name){
-        $argumentValue = explode('@',$name);
+    private function parseInstructionArgument($name)
+    {
+        $argumentValue = explode('@', $name);
         $valuesFound = count($argumentValue);
 
         // ...@...
-        if ($valuesFound == 2) 
-        {
-            if ($argumentValue[1] == '') 
+        if ($valuesFound == 2) {
+            if ($argumentValue[1] == '')
                 exit(SYNTAX_ERROR);
 
-            switch($argumentValue[0]){
+            switch ($argumentValue[0]) {
                 case 'GF':
                 case 'TF':
                 case 'LF':
                     return array('var', $name);
-                    
+
                 case 'int':
                 case 'bool':
                 case 'nil':
                     return array($argumentValue[0], $argumentValue[1]);
-               
+
                 case 'string':
                     return array($argumentValue[0], $this->parseString($argumentValue[1]));
-                            
+
                 default:
                     exit(LEXICAL_ERROR);
             }
-        // ...
+            // ...
         } else if ($valuesFound == 1) {
             switch ($argumentValue[0]) {
                 case 'int':
@@ -151,29 +160,31 @@ class mainParser{
                 default:
                     exit(LEXICAL_ERROR);
             }
-        // ... ... ... 
+            // ... ... ... 
         } else {
             exit(SYNTAX_ERROR);
         }
     }
 
-    private function instruction($instruction, $arg1 = false, $arg2 = false){
-        echo "\t<instruction order=\"".$this->instructionOrder++."\" opcode=\"".$instruction."\">\n";
-        
+    private function instruction($instruction, $arg1 = false, $arg2 = false)
+    {
+        echo "\t<instruction order=\"" . $this->instructionOrder++ . "\" opcode=\"" . $instruction . "\">\n";
+
         if ($arg1) {
             list($type, $arg1) = $this->parseInstructionArgument($arg1);
-            echo "\t\t<arg1 type=\"".$type."\">".$arg1."</arg1>\n";
-        }    
+            echo "\t\t<arg1 type=\"" . $type . "\">" . $arg1 . "</arg1>\n";
+        }
         if ($arg2) {
             list($type, $arg2) = $this->parseInstructionArgument($arg2);
-            echo "\t\t<arg2 type=\"".$type."\">".$arg2."</arg2>\n";
+            echo "\t\t<arg2 type=\"" . $type . "\">" . $arg2 . "</arg2>\n";
         }
 
         echo "\t</instruction>\n";
     }
 
 
-    private function prepareLine(){
+    private function prepareLine()
+    {
         $line = fgets($this->stdin);
         //EOF = false
         if ($line == false) {
@@ -182,15 +193,15 @@ class mainParser{
         }
 
         //remove any comment / whitespace
-        $commentPos = strpos($line,'#');
+        $commentPos = strpos($line, '#');
         if (is_numeric($commentPos)) {
-            $line = substr($line,0,$commentPos);
+            $line = substr($line, 0, $commentPos);
         }
         $line = trim($line);
 
 
         //EMPTY LINE / COMMENT ONLY -> get another
-        if ($line == null) 
+        if ($line == null)
             $this->prepareLine();
         else
             $this->curLine = $line;
@@ -198,27 +209,33 @@ class mainParser{
     }
 
 
-    private function parseBody(){
+    private function parseBody()
+    {
         $this->prepareLine();
 
         //EOF
-        if ($this->curLine == false) 
+        if ($this->curLine == false)
             return;
-        
+
         //parse input
-        $this->curLine = explode(" ",$this->curLine);
+        $this->curLine = explode(" ", $this->curLine);
         $this->curLine[0] = strtoupper($this->curLine[0]);
+        $argumentCount = count($this->curLine);
         switch ($this->curLine[0]) {
             case 'DEFVAR':
-                $this->instruction($this->curLine[0],$this->curLine[1]);
+                $this->instruction($this->curLine[0], $this->curLine[1]);
                 break;
             case 'READ':
-                $this->instruction($this->curLine[0],$this->curLine[1],$this->curLine[2]);
+                $this->instruction($this->curLine[0], $this->curLine[1], $this->curLine[2]);
                 break;
             case 'WRITE':
-                $this->instruction($this->curLine[0],$this->curLine[1]);
-                break;            
+                $this->instruction($this->curLine[0], $this->curLine[1]);
+                break;
             case 'CREATEFRAME':
+            case 'PUSHFRAME':
+            case 'POPFRAME':
+                if ($argumentCount != 1) 
+                    exit(SYNTAX_ERROR);
                 $this->instruction($this->curLine[0]);
                 break;
 
@@ -226,31 +243,33 @@ class mainParser{
                 // echo DEVEL."Operation ".$this->curLine[0]." not found \n\n";
                 exit(UNEXPECTED_COMMAND);
         }
+
+        //recursive calling
         $this->parseBody();
     }
 
-    public function parse($stdin){
+    public function parse($stdin)
+    {
         $this->stdin = $stdin;
 
         $this->header();
 
         $this->parseBody();
-        
+
         //finish
         echo "</program>\n";
         return;
     }
-
 }
 
 
-$argumentParser = new ArgumentParser($argc,$argv);
+$argumentParser = new ArgumentParser($argc, $argv);
 
 $argumentParser->parseArguments();
 
 $parser = new mainParser();
 
-$parser->parse(fopen("php://stdin","r"));
+$parser->parse(fopen("php://stdin", "r"));
 // $parser->parse(fopen("tests/header/blankLine.src","r"));
 
 exit(SUCCESS);
